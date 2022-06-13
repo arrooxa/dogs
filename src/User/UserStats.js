636e2f623a06b2f6f01e7f13a2a9da5749765160
Vitor Rocha <vitor.league@gmail.com>
2022-06-13 18:52:21 -0300

    Adding Graphs Library and Lazy/Suspense

--- src/User/UserStats.js
+++ src/User/UserStats.js
@@ -4,11 +4,18 @@ import Head from "../Helper/Head";
 import useFetch from "../Hooks/useFetch";
 import Error from "../Helper/Error";
 import Loading from "../Helper/Loading";
-import UserStatsGraph from "./UserStatsGraph";
 import Empty from "../Helper/Empty";
+const UserStatsGraph = React.lazy(() => import("./UserStatsGraph"));
+
+const data = [
+	{ id: 21984, title: "dog3", acessos: "2" },
+	{ id: 21982, title: "dog2", acessos: "1" },
+	{ id: 21980, title: "dog", acessos: "1" },
+	{ id: 21980, title: "dog", acessos: "3" },
+];
 
 const UserStats = () => {
-	const { data, loading, error, request } = useFetch();
+	const { loading, error, request } = useFetch();
 
 	React.useEffect(() => {
 		async function fetchData() {
@@ -28,7 +35,11 @@ const UserStats = () => {
 			{error && <Error error={error} />}
 			{loading && <Loading />}
 			{data && data.length === 0 && <Empty />}
-			{data && data.length > 0 && <UserStatsGraph data={data} />}
+			{data && data.length > 0 && (
+				<React.Suspense fallback={<Empty />}>
+					<UserStatsGraph data={data} />
+				</React.Suspense>
+			)}
 		</div>
 	);
 };
--- src/User/UserStatsGraph.js
+++ src/User/UserStatsGraph.js
@@ -1,7 +1,89 @@
 import React from "react";
+import styles from "./UserStatsGraph.module.css";
 
-const UserStatsGraph = () => {
-	return <div>UserStatsGraph</div>;
+import {
+	Chart as ChartJS,
+	ArcElement,
+	Tooltip,
+	Legend,
+	CategoryScale,
+	LinearScale,
+	BarElement,
+} from "chart.js";
+import { Doughnut, Bar } from "react-chartjs-2";
+
+ChartJS.register(
+	ArcElement,
+	Tooltip,
+	Legend,
+	CategoryScale,
+	LinearScale,
+	BarElement
+);
+
+const options = {
+	responsive: true,
+};
+
+const UserStatsGraph = ({ data }) => {
+	const [total, setTotal] = React.useState(0);
+	const [graphData, setGraphData] = React.useState([]);
+	const [labels, setLabels] = React.useState([]);
+
+	React.useEffect(() => {
+		const graphData = data.map((item) => Number(item.acessos));
+		setGraphData(graphData);
+
+		const graphLabels = data.map((item) => item.title);
+		setLabels(graphLabels);
+
+		const total = data
+			.map(({ acessos }) => Number(acessos))
+			.reduce((a, b) => a + b);
+		setTotal(total);
+	}, [data]);
+
+	return (
+		<section className={`${styles.graphs} animeLeft`}>
+			<div className={`${styles.total} ${styles.graphItem}`}>
+				<p>Total de Acessos: {total}</p>
+			</div>
+			<div className={styles.graphItem}>
+				<Doughnut
+					data={{
+						labels,
+						datasets: [
+							{
+								label: "Número de acessos individuais",
+								data: graphData,
+								backgroundColor: [
+									"rgba(245, 161, 39, 0.67)",
+									"rgba(0, 0, 0, 0.65)",
+								],
+								borderColor: "rgba(228, 227, 229, 0.39)",
+								borderWidth: 5,
+							},
+						],
+					}}
+				/>
+			</div>
+			<div className={styles.graphItem}>
+				<Bar
+					data={{
+						labels,
+						datasets: [
+							{
+								label: "Número de acessos individuais",
+								data: graphData,
+								backgroundColor: ["rgba(245, 161, 39, 0.67)"],
+							},
+						],
+					}}
+					options={options}
+				/>
+			</div>
+		</section>
+	);
 };
 
 export default UserStatsGraph;
